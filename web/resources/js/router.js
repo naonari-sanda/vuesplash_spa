784c0962e9a8809f57a468c03b0daff44a9e2c20
Nonari Sanda <mkg_sandy@icloud.com>
2021-05-03 17:24:57 +0900

    photo送信機能の修正をしました

--- web/resources/js/router.js
+++ web/resources/js/router.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// ページコンポーネントをインポートする
 import PhotoList from './pages/PhotoList.vue'
+import PhotoDetail from './pages/PhotoDetail.vue'
 import Login from './pages/Login.vue'
-
-import store from './store'
 import SystemError from './pages/errors/System.vue'
 
+import store from './store'
+
+// VueRouterプラグインを使用する
+// これによって<RouterView />コンポーネントなどを使うことができる
 Vue.use(VueRouter)
 
+// パスとコンポーネントのマッピング
 const routes = [{
     path: '/',
     component: PhotoList
   },
   {
-    path: '/500',
-    component: SystemError
+    path: '/photos/:id',
+    component: PhotoDetail,
+    props: true
   },
   {
     path: '/login',
@@ -27,12 +33,19 @@ const routes = [{
         next()
       }
     }
+  },
+  {
+    path: '/500',
+    component: SystemError
   }
 ]
 
+// VueRouterインスタンスを作成する
 const router = new VueRouter({
   mode: 'history',
   routes
 })
 
+// VueRouterインスタンスをエクスポートする
+// app.jsでインポートするため
 export default router

2177692faf39cf4df14eac80b215c737a86de3df
Nonari Sanda <mkg_sandy@icloud.com>
2021-05-06 19:45:37 +0900

    全体の修正をしました

--- web/routes/api.php
+++ web/routes/api.php
@@ -27,3 +27,8 @@ Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
 Route::post('/photos/{photo}/comments', 'PhotoController@addComment')->name('photo.comment');
 Route::put('/photos/{id}/like', 'PhotoController@like')->name('photo.like');
 Route::delete('/photos/{id}/like', 'PhotoController@unlike');
+Route::get('/reflesh-token', function (Illuminate\Http\Request $request) {
+    $request->session()->regenerateToken();
+
+    return response()->json();
+});
